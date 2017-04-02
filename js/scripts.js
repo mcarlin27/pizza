d799cd214cc990b4e1e3b3cf03e4d41395a227c5
Marilyn Carlin <marilyn.s.carlin@gmail.com>
2017-04-01 17:11:53 -0700

    add click listener to 'add delivery address' button to show address component of form once delivery address button is clicked.

--- js/scripts.js
+++ js/scripts.js
@@ -19,3 +19,31 @@ Address.prototype.fullAddress = function() {
 };
 
 // front-end logic
+$(document).ready(function() {
+  $("#add-address").click(function() {
+    $("#address").append('<div class="delivery-address">' +
+                            '<label for="addressType">Address Type</label>' +
+                            '<select class="form-control" id="addressType">' +
+                              '<option>House</option>' +
+                              '<option>Apartment</option>' +
+                              '<option>Office</option>' +
+                            '</select>' +
+                            '<br>' +
+                            '<div class="form-group">' +
+                              '<label for="street">Street</label>' +
+                              '<input type="text" class="form-control street">' +
+                            '</div>' +
+                            '<div class="form-group">' +
+                              '<label for="city">City</label>' +
+                              '<input type="text" class="form-control city">' +
+                            '</div>' +
+                            '<div class="form-group">' +
+                              '<label for="state">State</label>' +
+                              '<input type="text" class="form-control state">' +
+                            '</div>' +
+                            '<div class="form-group">' +
+                              '<label for="input">Special Instructions?</label>' +
+                              '<textarea id="input" class="form-control" rows="4" cols="10" placeholder="Enter text here"></textarea>' +
+                            '</div>' +
+                          '</div>');
+    });
